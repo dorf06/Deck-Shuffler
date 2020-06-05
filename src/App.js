f11216efc8096edbe9738312793a1651fc878c40
Cordell Wagendorf <dorf06@gmail.com>
2020-06-04 23:36:52 -0700

    Stopping for the night

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { DetailsList, DefaultButton } from '@fluentui/react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            deck: makeDeck()
+        }
+
+        makeDeck('hearts');
+    }
+
+    shuffleDeck(deck) {
+        let newDeck = deck.reverse();
+
+        this.setState({
+            deck: newDeck
+        })
+    }
+
+    render() {
+        console.log(this.state.deck);
+
+        return(
+            <>
+                <h1>Testing</h1>
+                <DefaultButton text="Shuffle" onClick={() => this.shuffleDeck(this.state.deck)}/>
+                <DetailsList items={this.state.deck} />
+            </>
+        );
+    }
+}
+
+function makeDeck() {
+    const suits = ['Hearts', 'Spades', 'Clubs', 'Diamonds'];
+
+    let deck = [];
+
+    suits.map(suit => 
+        [...new Array(13).keys()].map((card, index) => 
+            deck.push(
+                {
+                    suit: suit,
+                    value: index + 1
+                }
+            )    
+        )    
+    );
+
+    return deck;
 }
 
 export default App;
